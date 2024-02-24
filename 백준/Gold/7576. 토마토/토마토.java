cb81578ad7dc57cd9831c87ec56841e8b62ac6bd
jwooo <87631442+jwooo@users.noreply.github.com>
2024-02-24 15:01:24 +0900

    [Gold V] Title: 토마토, Time: 692 ms, Memory: 122852 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int max = 0;
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if (map[i][j] == 1) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            visited[now[0]][now[1]] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int nextY = now[0] + dy[i];
+                int nextX = now[1] + dx[i];
+
+                if (nextY >= 0 && nextY < n && nextX >= 0 && nextX < m) {
+                    if (!visited[nextY][nextX] && map[nextY][nextX] > -1) {
+                        visited[nextY][nextX] = true;
+                        map[nextY][nextX] = map[now[0]][now[1]] + 1;
+                        queue.add(new int[]{nextY, nextX});
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    System.out.println(-1);
+                    return;
+                } else {
+                    if (map[i][j] > max) max = map[i][j];
+                }
+            }
+        }
+
+        System.out.println(max - 1);
+    }
+}
