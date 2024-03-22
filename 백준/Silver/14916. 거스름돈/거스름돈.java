120f856506c6622b597369116b485355cc40222e
jwooo <87631442+jwooo@users.noreply.github.com>
2024-03-22 14:41:40 +0900

    [Silver V] Title: 거스름돈, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        while (n > 0) {
+            if (n % 5 == 0) {
+                count = n / 5 + count;
+                break;
+            }
+
+            n -= 2;
+            count++;
+        }
+
+        if (n < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(count);
+        }
+    }
+}
