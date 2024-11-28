fc3c83b70f442e869ed03f282a6846b6699c45b9
jwooo <87631442+jwooo@users.noreply.github.com>
2024-11-28 23:29:47 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 444 ms, Memory: 52464 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -1,54 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[] power;
-    static String[] title;
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        power = new int[N];
-        title = new String[N];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            
-            title[i] = st.nextToken();
-            power[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        for (int i = 0; i < M; i++) {
-            int key = Integer.parseInt(br.readLine());
-            binarySearch(key);
-        }
-        
-        System.out.println(sb);
-        
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
     }
 
-    static void binarySearch(int key) {
-        int left = 0;
-        int right = N;
-        int index = 0;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
-            if (key <= power[mid]) {
-                right = mid - 1;
-                index = mid;
-            } else if (key > power[mid]) {
-                left = mid + 1;
+    void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String[] powerKey = new String[n];
+        int[] powerValue = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            powerKey[i] = st.nextToken();
+            powerValue[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int findNumber = Integer.parseInt(br.readLine());
+            int start = 0;
+            int end = powerValue.length - 1;
+
+            while (start <= end) {
+                int mid = (start + end) / 2;
+
+                if (findNumber <= powerValue[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
+
+            sb.append(powerKey[start]).append("\n");
         }
-        
-        sb.append(title[index]).append("\n");
+
+        System.out.println(sb);
     }
-    
+
 }
