f29789309c94f8f29b55f52aec7e721eb2a3eee1
jwooo <87631442+jwooo@users.noreply.github.com>
2024-12-21 20:57:16 +0900

    [level 3] Title: 입국심사, Time: 106.91 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        
+        long start = 0;
+        long end = (long) times[times.length - 1] * n;
+        
+        while (start < end) {
+            long mid = (start + end) / 2;
+            long count = 0;
+            
+            for (int i = 0; i < times.length; i++) {
+                count += mid / times[i];
+                if (count > n) break;
+            }
+            
+            if (n <= count) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return start;
+    }
+}
