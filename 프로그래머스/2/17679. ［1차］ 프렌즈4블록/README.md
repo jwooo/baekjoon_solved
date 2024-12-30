daa1f432c6e3227335ad1280bc27a81864c2ef2d
jwooo <87631442+jwooo@users.noreply.github.com>
2024-12-30 21:07:15 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 16.97 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 프렌즈4블록 - 17679 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 35.56 ms
+메모리: 75.8 MB, 시간: 16.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 30일 17:47:00
+2024년 12월 30일 21:07:12
 
 ### 문제 설명
 
--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,90 +1,65 @@
 import java.util.*;
 
 class Solution {
-    
-    static int[] dx = {1, 1, 0};
-    static int[] dy = {0, 1, 1};
-    
     public int solution(int m, int n, String[] board) {
         int answer = 0;
         boolean isDone = false;
-        boolean[][] poped = new boolean[m][n];
         char[][] map = new char[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        boolean[][] poped = new boolean[m][n];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length(); j++) {
                 map[i][j] = board[i].charAt(j);
             }
         }
-        
-        
+
         while (!isDone) {
             boolean isPoped = false;
-            
+
             for (int i = 0; i < m - 1; i++) {
                 for (int j = 0; j < n - 1; j++) {
                     char now = map[i][j];
-                    
-                    if (now != ' ') {
-                        int sameCount = 0;
-                        for (int k = 0; k < 3; k++) {
-                            char block = map[i + dx[k]][j + dy[k]];
-                            
-                            if (now == block) sameCount++;
-                        }
-                        
-                        if (sameCount == 3) {
+
+                    if (now == '*') continue;
+                    else {
+                        if (now == map[i][j + 1] && now == map[i + 1][j] && now == map[i + 1][j + 1]) {
                             isPoped = true;
                             poped[i][j] = true;
                             poped[i][j + 1] = true;
                             poped[i + 1][j] = true;
                             poped[i + 1][j + 1] = true;
                         }
-                    }
+                    } 
                 }
             }
-            
-            
+
             if (isPoped) {
-                for (int i = m - 1; i >= 0; i--) {
-                    for (int j = n - 1; j >= 0; j--) {
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
                         if (poped[i][j]) {
-                            for (int k = i - 1; k >= 0; k--) {
-                                if (map[k][j] != ' ' && !poped[k][j]) {
-                                    poped[i][j] = false;
-                                    poped[k][j] = true;
-                                    map[i][j] = map[k][j];
-                                    map[k][j] = ' ';
-                                    break;
-                                }
-                            }
-                            
-                            if (poped[i][j]) {
-                                map[i][j] = ' ';
-                                poped[i][j] = false;
-                            }
+                            map[i][j] = '*';
+                            poped[i][j] = false;
+                            answer++;
                         }
                     }
                 }
                 
-            } else {
-                isDone = true;
-            }
+                for (int i = m - 1; i >= 0; i--) {
+                    for (int j = 0; j < n; j++) {
+                        if (map[i][j] == '*') {
+                            for (int k = i - 1; k >= 0; k--) {
+                                if (map[k][j] != '*') {
+                                    map[i][j] = map[k][j];
+                                    map[k][j] = '*';
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            } else isDone = true;
         }
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == ' ') answer++;
-            }
-        }
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        
+
         return answer;
     }
 }
