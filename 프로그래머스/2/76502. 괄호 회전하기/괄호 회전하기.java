1328638571baea38b41c4e5b9193595653255fc6
jwooo <87631442+jwooo@users.noreply.github.com>
2025-01-04 16:40:39 +0900

    [level 2] Title: 괄호 회전하기, Time: 140.54 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,31 +4,29 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         Stack<Character> stack = new Stack<>();
-        List<Character> now = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         
         for (int i = 0; i < s.length(); i++) {
-            now.add(s.charAt(i));
+            sb.append(s.charAt(i));
         }
         
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < s.length(); j++) {
-                Character nowChar = now.get(j);
+            
+            String now = sb.toString();
+            for (int j = 0; j < now.length(); j++) {
+                Character nowValue = now.charAt(j);
                 
-                if (stack.isEmpty()) {
-                    stack.push(nowChar);
-                } else if (stack.peek() == '(' && nowChar == ')') {
-                    stack.pop();
-                } else if (stack.peek() == '{' && nowChar == '}') {
-                    stack.pop();
-                } else if (stack.peek() == '[' && nowChar == ']') {
-                    stack.pop();
-                } else {
-                    stack.push(nowChar);
-                }
+                if (stack.isEmpty()) stack.push(nowValue);
+                else if (stack.peek() == '(' && nowValue == ')') stack.pop();
+                else if (stack.peek() == '{' && nowValue == '}') stack.pop();
+                else if (stack.peek() == '[' && nowValue == ']') stack.pop();
+                else stack.push(nowValue);
             }
             
             if (stack.isEmpty()) answer++;
-            now.add(now.remove(0));
+            
+            sb.append(sb.charAt(0));
+            sb.deleteCharAt(0);
             stack.clear();
         }
         
