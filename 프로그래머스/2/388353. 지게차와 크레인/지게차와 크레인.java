e6c47795dc5bcf55a082931cb7c56a0db4b5216a
jwooo <87631442+jwooo@users.noreply.github.com>
2025-02-12 21:19:21 +0900

    [level 2] Title: 지게차와 크레인, Time: 66.86 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 class Solution {
     
+    private static int n = 0;
+    private static int m = 0;
     private static final int[] dx = {0, 1, 0, -1};
     private static final int[] dy = {-1, 0, 1, 0};
     
     public int solution(String[] storage, String[] requests) {
         int answer = 0;
         
-        String[][] map = new String[storage.length][storage[0].length()];
-        boolean[][] visited = new boolean[storage.length][storage[0].length()];
+        n = storage.length;
+        m = storage[0].length();
+        
+        String[][] map = new String[n][m];
+        boolean[][] visited = new boolean[n][m];
         
         for (int i = 0; i < map.length; i++) {
             map[i] = storage[i].split("");
@@ -21,7 +26,10 @@ class Solution {
             if (request.length() == 1) pullContainers = useForkLift(request, map, visited);
             else pullContainers = useCrane(String.valueOf(request.charAt(0)), map, visited);
             
-            pullContainers.forEach(idx -> visited[idx[0]][idx[1]] = true);
+            pullContainers.forEach(idx -> {
+                map[idx[0]][idx[1]] = ".";
+                visited[idx[0]][idx[1]] = true;
+            });
         }
         
         for (int i = 0; i < visited.length; i++) {
@@ -39,40 +47,36 @@ class Solution {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
                 if (map[i][j].equals(container) && !visited[i][j]) {
-                    boolean[][] visitedCopy = Arrays.stream(visited)
-                        .map(row -> Arrays.copyOf(row, row.length))
-                        .toArray(boolean[][]::new);
-                    
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[] {i, j});
-                    
-                    boolean isOutContainer = false;
-                    while (!queue.isEmpty()) {
-                        int[] now = queue.poll();
-                        
-                        for (int k = 0; k < 4; k++) {
-                            int nowY = now[0] + dy[k];
-                            int nowX = now[1] + dx[k];
-                            
-                            if (nowX < 0 || nowX >= map[0].length || nowY < 0 || nowY >= map.length) {
-                                isOutContainer = true;
-                                pullContainers.add(new int[] {i, j});
-                                break;
-                            } 
-                            
-                            if (visitedCopy[nowY][nowX]) {
-                                visitedCopy[nowY][nowX] = false;
-                                queue.offer(new int[] {nowY, nowX});
-                            }
-                        }
-                        
-                        if (isOutContainer) break;
-                    }
+                    if (isContainerAccessible(i, j, map)) pullContainers.add(new int[] {i, j});
                 }
             }
         }
         
         return pullContainers;
+    }
+    
+    private boolean isContainerAccessible(int startY, int startX, String[][] map) {
+        boolean[][] visited = new boolean[n][m];
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {startY, startX});
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nowY = now[0] + dy[i];
+                int nowX = now[1] + dx[i];
+
+                if (nowX < 0 || nowX >= map[0].length || nowY < 0 || nowY >= map.length) return true;
+                if (map[nowY][nowX].equals(".") && !visited[nowY][nowX]) {
+                    visited[nowY][nowX] = true;
+                    queue.offer(new int[] {nowY, nowX});
+                }
+            }
+        }
+
+        return false;
     }
     
     private List<int[]> useCrane(String container, String[][] map, boolean[][] visited) {
@@ -86,4 +90,5 @@ class Solution {
         
         return pullContainers;
     }
+    
 }
