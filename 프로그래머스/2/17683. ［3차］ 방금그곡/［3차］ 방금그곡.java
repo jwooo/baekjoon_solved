e9e94ced854a619a4ae550d1ed47fd213e15c8fe
jwooo <87631442+jwooo@users.noreply.github.com>
2025-03-02 20:07:13 +0900

    [level 2] Title: [3차] 방금그곡, Time: 13.64 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -2,73 +2,58 @@ import java.util.*;
 
 class Solution {
     public String solution(String m, String[] musicinfos) {
-        String melody = replaceSharp(m);
-        List<Music> musics = new ArrayList<>();
-
-        for (int i = 0; i < musicinfos.length; i++) {
-            String[] musicInfo = musicinfos[i].split(",");
-
-            int musicTime = getTimeDifference(musicInfo[0], musicInfo[1]);
-            String musicName = musicInfo[2];
-            String musicMelody = musicInfo[3];
+        String answer = "(None)";
+        int maxPlayTime = 0;
+        m = replaceSharp(m);
+        
+        for (String info : musicinfos) {
+            String[] parts = info.split(",");
+            String start = parts[0];
+            String end = parts[1];
+            String title = parts[2];
+            String melody = replaceSharp(parts[3]);
             
-            int index = 0;
-            int count = musicTime;
-            StringBuilder sb = new StringBuilder();
-            while (count > 0) {
-                sb.append(musicMelody.charAt(index % musicMelody.length()));
-
-                char next = (musicMelody.charAt((index + 1) % musicMelody.length()));
-                if (next == '#') {
-                    sb.append(next);
-                    index++;
+            int playTime = getTimeDifference(start, end);
+            String playedMelody = getPlayedMelody(melody, playTime);
+            
+            if (playedMelody.contains(m)) {
+                if (playTime > maxPlayTime) {
+                    maxPlayTime = playTime;
+                    answer = title;
                 }
-
-                index++;
-                count--;
             }
-
-            String playedMusic = replaceSharp(sb.toString());
-            if (playedMusic.contains(melody)) musics.add(new Music(musicName, musicTime));
         }
-
-        if (musics.isEmpty()) return "(None)";
-        Collections.sort(musics);
-
-        return musics.get(0).name;
+        
+        return answer;
+    }
+    
+    private String getPlayedMelody(String melody, int playTime) {
+        int size = melody.length();
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < playTime; i++) {
+            sb.append(melody.charAt(i % size));
+        }
+        
+        return sb.toString();
     }
     
     private int getTimeDifference(String startTime, String endTime) {
         return extractTime(endTime) - extractTime(startTime);
     }
-
+    
     private int extractTime(String strTime) {
         String[] times = strTime.split(":");
         return Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
     }
-
+    
     private String replaceSharp(String melody) {
-        return melody.replaceAll("C#", "c")
+        return melody.replaceAll("A#", "a")
+            .replaceAll("B#", "b")
+            .replaceAll("C#", "c")
             .replaceAll("D#", "d")
-            .replaceAll("F#", "f")
-            .replaceAll("G#", "g")
-            .replaceAll("A#", "a")
             .replaceAll("E#", "e")
-            .replaceAll("B#", "b");
-    }
-
-    static class Music implements Comparable<Music> {
-        String name;
-        int time;
-        
-        public Music(String name, int time) {
-            this.name = name;
-            this.time = time;
-        }
-
-        @Override
-        public int compareTo(Music other) {
-            return other.time - this.time;
-        }
+            .replaceAll("F#", "f")
+            .replaceAll("G#", "g");
     }
 }
