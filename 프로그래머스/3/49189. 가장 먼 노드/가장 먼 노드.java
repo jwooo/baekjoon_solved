1b4a2560a32c73f2e77b8bdb309c0cd53c32cf15
jwooo <87631442+jwooo@users.noreply.github.com>
2025-06-11 15:49:34 +0900

    [level 3] Title: 가장 먼 노드, Time: 87.42 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        int max = 0;
+        int[] distance = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
         List<List<Edge>> graph = new ArrayList<>();
-        boolean[] visited = new boolean[edge.length + 1];
+        
+        Arrays.fill(distance, Integer.MAX_VALUE);
         
         for (int i = 0; i < edge.length; i++) {
             graph.add(new ArrayList<>());
@@ -15,33 +17,36 @@ class Solution {
             int startNode = edge[i - 1][0];
             int endNode = edge[i - 1][1];
             
-            graph.get(startNode).add(new Edge(endNode, 0));
-            graph.get(endNode).add(new Edge(startNode, 0));
+            graph.get(startNode).add(new Edge(endNode, 1));
+            graph.get(endNode).add(new Edge(startNode, 1));
         }
         
         Queue<Edge> queue = new LinkedList<>();
         queue.offer(new Edge(1, 0));
+        distance[0] = distance[1] = 0;
         visited[1] = true;
         
         while (!queue.isEmpty()) {
             Edge now = queue.poll(); 
             
+            if (now.weight < distance[now.node]) {
+                distance[now.node] = now.weight;
+            }
+            
             for (Edge next : graph.get(now.node)) {
-                int nextWeight = now.weight + 1;
+                int nextWeight = now.weight + next.weight;
                 
-                if (!visited[next.node]) {
-                    visited[next.node] = true;
-                    
-                    if (nextWeight > max) {
-                        max = nextWeight;
-                        answer = 1;
-                    } else if (nextWeight == max) {
-                        answer++;
-                    }
-                    
+                if (distance[next.node] > nextWeight) {
+                    distance[next.node] = nextWeight;
                     queue.offer(new Edge(next.node, nextWeight));
                 }
             }
+        }
+        
+        Arrays.sort(distance);
+        int max = distance[n];
+        for (int i = n; i >= 0; i--) {
+            if (max == distance[i]) answer++;
         }
         
         return answer;
