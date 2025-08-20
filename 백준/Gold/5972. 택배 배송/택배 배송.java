ebbc5ba7bae7dea3a43b4d8150079c790f6d5ac3
jwooo <87631442+jwooo@users.noreply.github.com>
2025-08-20 14:54:28 +0900

    [Gold V] Title: 택배 배송, Time: 380 ms, Memory: 41472 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<List<Edge>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph.get(start).add(new Edge(end, weight));
+            graph.get(end).add(new Edge(start, weight));
+        }
+
+        int answer = dijkstra(1, n, graph);
+        System.out.println(answer);
+    }
+
+    private int dijkstra(int start, int end, List<List<Edge>> graph) {
+        int[] distance = new int[graph.size()];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+
+            if (distance[now.node] < now.weight) {
+                continue;
+            }
+
+            for (Edge next : graph.get(now.node)) {
+                int dist = now.weight + next.weight;
+
+                if (distance[next.node] > dist) {
+                    distance[next.node] = dist;
+                    pq.offer(new Edge(next.node, dist));
+                }
+            }
+        }
+
+        return distance[end];
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int node;
+        int weight;
+
+        public Edge(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
+        }
+    }
+
+}
