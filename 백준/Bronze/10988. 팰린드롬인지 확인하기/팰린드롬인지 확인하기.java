37f2dbe21fe6e3e1a8bd15638b8c07748c64c18f
jwooo <87631442+jwooo@users.noreply.github.com>
2025-08-30 21:53:36 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,23 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		sc.close();
-		int len = str.length();
-		int ans = 1;
-		
-		StringBuilder sb = new StringBuilder(str);
-		sb.reverse();
-		
-		for(int i=0;i<len/2;i++) {
-			if(str.charAt(i) != sb.charAt(i)) {
-				ans = 0;
-			}
-		}
-		System.out.println(ans);
-		
-	}
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+        char[] arr = br.readLine().toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+        int answer = 1;
+
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                answer = 0;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println(answer);
+    }
 }
