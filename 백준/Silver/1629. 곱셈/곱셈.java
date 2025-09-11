962f2af82e2013e43f19d18ae7f1b741f5ac548a
jwooo <87631442+jwooo@users.noreply.github.com>
2025-09-11 21:54:41 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(power(A, B, C));
+    }
+
+    public static long power(long A, long B, long C) {
+        if (B == 1) return A % C;
+
+        long temp = power(A, B / 2, C);
+        long result = (temp * temp) % C;
+
+        if (B % 2 == 1) result = (result * A) % C;
+        return result;
+    }
+}
