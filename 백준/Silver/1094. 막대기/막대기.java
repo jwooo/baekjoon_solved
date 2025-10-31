a0a09170457ba411433533a5d8325b63f0360b06
jwooo <87631442+jwooo@users.noreply.github.com>
2025-10-31 22:21:43 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public Main() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+        int x = Integer.parseInt(br.readLine());
+        int count = Integer.bitCount(x);
+
+        System.out.println(count);
+    }
+}
