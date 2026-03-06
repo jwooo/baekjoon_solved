d321f195b84aeb248ee9ee3b50be5f59bb7ed550
jwooo <87631442+jwooo@users.noreply.github.com>
2026-03-06 16:29:05 +0900

    [Gold IV] Title: 키 순서, Time: 604 ms, Memory: 37572 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            dist[start][end] = 1;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] == 1 && dist[k][j] == 1) {
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (dist[i][j] == 1 || dist[j][i] == 1) {
+                    count++;
+                }
+            }
+
+            if (count == n - 1) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
