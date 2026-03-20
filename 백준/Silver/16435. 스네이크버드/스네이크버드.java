bd51279b3311682f4b54b0f7ef7ea2a98a5f9ccf
jwooo <87631442+jwooo@users.noreply.github.com>
2026-03-20 17:32:15 +0900

    [Silver V] Title: 스네이크버드, Time: 112 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(getMaxLength(l, arr));
+    }
+
+    private int getMaxLength(int l, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (l >= arr[i]) {
+                l++;
+            } else {
+                return l;
+            }
+        }
+
+        return l;
+    }
+
+}
