eb5048df23ee8ecdbeb425af12d7bcdf73310658
jwooo <87631442+jwooo@users.noreply.github.com>
2026-03-27 16:42:42 +0900

    [Gold V] Title: 수 고르기, Time: 316 ms, Memory: 28640 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = 0;
+        int min = Integer.MAX_VALUE;
+
+        while (left < n && right < n) {
+            if (arr[right] - arr[left] >= m) {
+                min = Math.min(min, arr[right] - arr[left]);
+                left++;
+            } else {
+                right++;
+            }
+        }
+
+        System.out.println(min);
+    }
+
+}
