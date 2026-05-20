90a121d20fdd909e1cc159a6a4a3e037aa5b4661
jwooo <87631442+jwooo@users.noreply.github.com>
2026-05-20 23:17:12 +0900

    [level 1] Title: 추억 점수, Time: 1.19 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                String now = photo[i][j];
+                
+                if (!map.containsKey(now)) continue;
+                answer[i] += map.get(now);
+            }
+        }
+        
+        return answer;
+    }
+}
