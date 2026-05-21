c9a5540dd92286560535ff1b120b22d0cf95d592
jwooo <87631442+jwooo@users.noreply.github.com>
2026-05-21 15:41:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        
+        while (num > 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
+            }
+            
+            answer++;
+        }
+        
+        if (num != 1) return -1;
+        return answer;
+    }
+}
