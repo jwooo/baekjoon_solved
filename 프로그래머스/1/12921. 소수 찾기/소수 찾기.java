41c91cefd6a32ce0f4eb1b5f809255f7a3847793
jwooo <87631442+jwooo@users.noreply.github.com>
2026-06-03 22:43:36 +0900

    [level 1] Title: 소수 찾기, Time: 10.34 ms, Memory: 59.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n) {
+        boolean[] prime = new boolean[n + 1];
+        
+        for (int i = 2; i <= n; i++) {
+            prime[i] = true;
+        }
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (prime[i]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
