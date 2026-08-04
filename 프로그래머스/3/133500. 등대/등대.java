81fbdfd7b5ef988bda33f9141114276b851f8f34
jwooo <87631442+jwooo@users.noreply.github.com>
2026-08-04 15:49:03 +0900

    [level 3] Title: 등대, Time: 55.77 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+
+    List<Integer>[] graph;
+    int[][] dp;
+    boolean[] visited;
+
+    public int solution(int n, int[][] lighthouse) {
+
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : lighthouse) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        dp = new int[n + 1][2];
+        visited = new boolean[n + 1];
+
+        dfs(1);
+
+        return Math.min(dp[1][0], dp[1][1]);
+    }
+
+    private void dfs(int now) {
+        visited[now] = true;
+
+        dp[now][1] = 1;
+
+        for (int next : graph[now]) {
+            if (visited[next]) continue;
+
+            dfs(next);
+
+            dp[now][0] += dp[next][1];
+            dp[now][1] += Math.min(dp[next][0], dp[next][1]);
+        }
+    }
+}
